c0e441b54455b07769f77789288cf0ea24f76402
wendyyanto <yanto.wendy98@gmail.com>
2021-08-08 18:31:54 +0700

    feature: setup more fields

--- src/main/kotlin/com/dev/wendyyanto/personalweb/model/entity/Profile.kt
+++ src/main/kotlin/com/dev/wendyyanto/personalweb/model/entity/Profile.kt
@@ -2,10 +2,28 @@ package com.dev.wendyyanto.personalweb.model.entity
 
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
+import org.springframework.data.mongodb.core.mapping.Field
 
 @Document(collection = "profile")
-class Profile(
+data class Profile(
     @Id
     val id: String,
-    val name: String
+    val name: String? = null,
+    val description: String? = null,
+    val fullDescription: String? = null,
+    val profileImageUrl: String? = null,
+    val contact: Contact? = null)
+
+data class Contact(
+    @Field("email")
+    val email: String? = null,
+
+    @Field("instagram")
+    val instagramLink: String? = null,
+
+    @Field("linkedin")
+    val linkedinLink: String? = null,
+
+    @Field("github")
+    val githubLink: String? = null
 )
